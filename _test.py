b21a5d2dc5b7c65dd3e4934c0bd33dc22a44406e
admin <023>
2023-09-08 14:28:29 +0300

    add fixture VictimEnv and use!

--- _test.py
+++ _test.py
@@ -3,21 +3,28 @@ import pytest
 from typing import *
 from private_values import PrivateValues, Exx_PvNotAccepted
 
+pass    # ENV =========================================================================================================
+pass    # ENV =========================================================================================================
+pass    # ENV =========================================================================================================
+pass    # ENV =========================================================================================================
+
 
 # PREPARE VICTIM ======================================================================================================
-class VictimRaise(PrivateValues):
+class VictimEnvCls(PrivateValues):
+    PV__USE_RC = False
     pass
 
 
-class VictimNoRaise(PrivateValues):
-    PV__RISE_EXCEPTION_IF_NONE = False
+@pytest.fixture()
+def VictimEnv():
+    VictimEnvCls._cls_set_defaults()
+    yield VictimEnvCls
 
 
 # =====================================================================================================================
 class Test1_Env:
     VALUE_DEF: str = "VALUE_DEF"
     VALUE_ENV: str = "VALUE_ENV"
-    VALUE_RC: str = "VALUE_RC"
 
     env_name__NotExists_short: str = "NotExists"
     env_name__NotExists_full: str = f"{PrivateValues.PV__PREFIX}{env_name__NotExists_short}"
@@ -50,93 +57,123 @@ class Test1_Env:
         del os.environ[cls.env_name__Exists_short]
 
     # PVS__RISE_EXCEPTION_IF_NONE -------------------------------------------------------------------------------------
-    def test__PVS__RISE_EXCEPTION_IF_NONE__True(self):
-        Victim = VictimRaise
-        setattr(Victim, self.env_name__NotExists_full, None)
+    def test__PVS__RISE_EXCEPTION_IF_NONE__True(self, VictimEnv):
+        setattr(VictimEnv, self.env_name__NotExists_full, None)
 
         try:
-            Victim()
+            VictimEnv()
         except Exx_PvNotAccepted:
             return
 
         assert False
 
-    def test__PVS__RISE_EXCEPTION_IF_NONE__False(self):
-        Victim = VictimNoRaise
-        setattr(Victim, self.env_name__NotExists_full, None)
+    def test__PVS__RISE_EXCEPTION_IF_NONE__False(self, VictimEnv):
+        setattr(VictimEnv, "PV__RISE_EXCEPTION_IF_NONE", False)
+        setattr(VictimEnv, self.env_name__NotExists_full, None)
 
         try:
-            Victim()
+            VictimEnv()
         except Exx_PvNotAccepted:
             assert False
 
     # ENV - NOT EXISTS ------------------------------------------------------------------------------------------------
-    def test__NoExists_None(self):
-        Victim = VictimRaise
-        setattr(Victim, self.env_name__NotExists_full, None)
+    def test__NoExists_None(self, VictimEnv):
+        setattr(VictimEnv, self.env_name__NotExists_full, None)
 
         try:
-            Victim()
+            VictimEnv()
         except Exx_PvNotAccepted:
             return
 
         assert False
 
-    def test__NoExists_NoNone(self):
-        Victim = VictimRaise
-        setattr(Victim, self.env_name__NotExists_full, self.VALUE_DEF)
+    def test__NoExists_NoNone(self, VictimEnv):
+        setattr(VictimEnv, self.env_name__NotExists_full, self.VALUE_DEF)
 
-        assert getattr(Victim(), self.env_name__NotExists_full) == self.VALUE_DEF
+        assert getattr(VictimEnv(), self.env_name__NotExists_full) == self.VALUE_DEF
 
     # ENV - EXISTS ----------------------------------------------------------------------------------------------------
-    def test__Exists_None(self):
-        Victim = VictimRaise
-        setattr(Victim, self.env_name__Exists_full, None)
+    def test__Exists_None(self, VictimEnv):
+        setattr(VictimEnv, self.env_name__Exists_full, None)
 
-        assert getattr(Victim(), self.env_name__Exists_full) == self.VALUE_ENV
+        assert getattr(VictimEnv(), self.env_name__Exists_full) == self.VALUE_ENV
 
-    def test__Exists_NoNone(self):
-        Victim = VictimRaise
-        setattr(Victim, self.env_name__Exists_full, self.VALUE_DEF)
+    def test__Exists_NoNone(self, VictimEnv):
+        setattr(VictimEnv, self.env_name__Exists_full, self.VALUE_DEF)
 
-        assert getattr(Victim(), self.env_name__Exists_full) == self.VALUE_ENV
+        assert getattr(VictimEnv(), self.env_name__Exists_full) == self.VALUE_ENV
 
     # _pvs_detected --------------------------------------------------------------------------------------------------
-    def test__envs_detected(self):
-        Victim = VictimRaise
-        setattr(Victim, self.env_name__NotExists_full, self.VALUE_DEF)
-        setattr(Victim, self.env_name__Exists_full, self.VALUE_DEF)
+    def test__envs_detected(self, VictimEnv):
+        setattr(VictimEnv, self.env_name__NotExists_full, self.VALUE_DEF)
+        setattr(VictimEnv, self.env_name__Exists_full, self.VALUE_DEF)
 
-        assert Victim()._pv_detected == {
+        assert VictimEnv()._pv_detected == {
             self.env_name__NotExists_short: self.VALUE_DEF,
             self.env_name__Exists_short: self.VALUE_ENV,
         }
-        setattr(Victim, self.env_name__NotExists_full, self.VALUE_DEF)
-        setattr(Victim, self.env_name__Exists_full, None)
+        setattr(VictimEnv, self.env_name__NotExists_full, self.VALUE_DEF)
+        setattr(VictimEnv, self.env_name__Exists_full, None)
 
-        assert Victim()._pv_detected == {
+        assert VictimEnv()._pv_detected == {
             self.env_name__NotExists_short: self.VALUE_DEF,
             self.env_name__Exists_short: self.VALUE_ENV,
         }
 
     # envs__show* ------------------------------------------------------------------------------------------------------
-    def test__pvs__show_detected(self):
-        Victim = VictimRaise
+    def test__pvs__show_detected(self, VictimEnv):
+        setattr(VictimEnv, self.env_name__NotExists_full, self.VALUE_DEF)
+        setattr(VictimEnv, self.env_name__Exists_full, None)
 
-        setattr(Victim, self.env_name__NotExists_full, self.VALUE_DEF)
-        setattr(Victim, self.env_name__Exists_full, None)
-
-        assert Victim().pv__show_detected() == {
+        assert VictimEnv().pv__show_detected() == {
             self.env_name__NotExists_short: self.VALUE_DEF,
             self.env_name__Exists_short: self.VALUE_ENV,
         }
 
-    def test__pvs__show_os_env(self):
-        Victim = VictimRaise
+    def test__pvs__show_os_env(self, VictimEnv):
         # uppercase - see docstring for method!
-        envs = Victim._pv__show_env(self.env_name__Exists_short)
+        envs = VictimEnv._pv__show_env(self.env_name__Exists_short)
         print(envs)
         assert envs.get(self.env_name__Exists_short.upper()) == self.VALUE_ENV
+
+
+pass    # RC ==========================================================================================================
+pass    # RC ==========================================================================================================
+pass    # RC ==========================================================================================================
+pass    # RC ==========================================================================================================
+
+
+# PREPARE VICTIM ======================================================================================================
+class VictimRaise_RC(PrivateValues):
+    PV__USE_ENV = False
+
+
+class VictimNoRaise_RC(PrivateValues):
+    PV__USE_ENV = False
+    PV__RISE_EXCEPTION_IF_NONE = False
+
+
+# =====================================================================================================================
+class Test2_Rc:
+    VALUE_DEF: str = "VALUE_DEF"
+    VALUE_RC: str = "VALUE_RC"
+
+    @classmethod
+    def setup_class(cls):
+
+
+        print()
+        print()
+        print(f"{cls.env_name__NotExists_short=}")
+        print(f"{cls.env_name__NotExists_full=}")
+        print(f"{cls.env_name__Exists_short=}")
+        print(f"{cls.env_name__Exists_full=}")
+        print()
+        print()
+
+    @classmethod
+    def teardown_class(cls):
+        pass
 
 
 # =====================================================================================================================
