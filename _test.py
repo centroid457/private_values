ec37b26e95cc6b4dc3dfb8ef58587298a086ea4c
admin <023>
2023-09-10 09:48:50 +0300

    correct tests and add tests for two objects

--- _test.py
+++ _test.py
@@ -53,9 +53,10 @@ class Test__env_value_get:
 # =====================================================================================================================
 class Test__IniValues:
     VICTIM: Type[IniValues] = type("VICTIM", (IniValues, ), {})
+    VICTIM2: Type[IniValues] = type("VICTIM2", (IniValues, ), {"FILENAME": f"{IniValues.FILENAME}2"})
     DIRPATH: pathlib.Path = pathlib.Path(TemporaryDirectory().name)
 
-    TEXT: str = f"""
+    TEXT1: str = f"""
 [DEFAULT]
 name=valueDef
 name0=valueDef
@@ -64,10 +65,20 @@ name0=valueDef
 name=value1
 name1=value1
     """
+    TEXT2: str = f"""
+[DEFAULT]
+name=valueDef2
+name0=valueDef2
+
+[SEC1]
+name=value12
+name1=value12
+        """
     @classmethod
     def setup_class(cls):
         cls.DIRPATH.mkdir()
-        cls.DIRPATH.joinpath(cls.VICTIM.FILENAME).write_text(cls.TEXT)
+        cls.DIRPATH.joinpath(cls.VICTIM.FILENAME).write_text(cls.TEXT1)
+        cls.DIRPATH.joinpath(cls.VICTIM2.FILENAME).write_text(cls.TEXT2)
 
     @classmethod
     def teardown_class(cls):
@@ -75,13 +86,15 @@ name1=value1
 
     def setup_method(self, method):
         self.VICTIM = type("VICTIM", (IniValues, ), {})
+        self.VICTIM2 = type("VICTIM2", (IniValues, ), {"FILENAME": f"{IniValues.FILENAME}2"})
         self.VICTIM.DIRPATH = self.DIRPATH
+        self.VICTIM2.DIRPATH = self.DIRPATH
 
     def test__notExist_file(self):
         self.VICTIM.FILENAME = "12345.ini"
 
         try:
-            self.VICTIM().get("name")
+            self.VICTIM.get("name")
         except Exx_PvNotAccepted:
             return
 
@@ -89,20 +102,30 @@ name1=value1
 
     def test__notExist_name(self):
         try:
-            self.VICTIM().get("name999")
+            self.VICTIM.get("name999")
         except Exx_PvNotAccepted:
             return
 
         assert False
 
     def test__Exist_name(self):
-        assert self.VICTIM().get("name", _raise_exx=False) == "valueDef"     #1
-        assert self.VICTIM().get("name0", _raise_exx=False) == "valueDef"    #2
-        assert self.VICTIM().get("name1", _raise_exx=False) is None          #3
+        # VICTIM1
+        assert self.VICTIM.get("name", _raise_exx=False) == "valueDef"
+        assert self.VICTIM.get("name0", _raise_exx=False) == "valueDef"
+        assert self.VICTIM.get("name1", _raise_exx=False) is None
 
-        assert self.VICTIM().get("name", section="SEC1", _raise_exx=False) == "value1"       #4
-        assert self.VICTIM().get("name0", section="SEC1", _raise_exx=False) == "valueDef"    #5
-        assert self.VICTIM().get("name1", section="SEC1", _raise_exx=False) == "value1"      #6
+        assert self.VICTIM.get("name", section="SEC1", _raise_exx=False) == "value1"
+        assert self.VICTIM.get("name0", section="SEC1", _raise_exx=False) == "valueDef"
+        assert self.VICTIM.get("name1", section="SEC1", _raise_exx=False) == "value1"
+
+        # VICTIM2
+        assert self.VICTIM2.get("name", _raise_exx=False) == "valueDef2"
+        assert self.VICTIM2.get("name0", _raise_exx=False) == "valueDef2"
+        assert self.VICTIM2.get("name1", _raise_exx=False) is None
+
+        assert self.VICTIM2.get("name", section="SEC1", _raise_exx=False) == "value12"
+        assert self.VICTIM2.get("name0", section="SEC1", _raise_exx=False) == "valueDef2"
+        assert self.VICTIM2.get("name1", section="SEC1", _raise_exx=False) == "value12"
 
 
 # =====================================================================================================================
