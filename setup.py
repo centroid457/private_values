404f22eb122bbcd23b49a6323c14398c02eb37d6
admin <023>
2023-09-11 16:23:40 +0300

    separate EDIT block in SETUP

--- setup.py
+++ setup.py
@@ -1,34 +1,32 @@
 from setuptools import setup, find_packages
 
-NAME = "private_values"
 
 with open("README.md", "r") as f:
     readme = f.read()
 
+# EDIT ================================================================================================================
+# EDIT ================================================================================================================
+# EDIT ================================================================================================================
+# EDIT ================================================================================================================
+# EDIT ================================================================================================================
+NAME = "private_values"
+
 setup(
-  name=NAME,
-  version="0.2.0.1",
-  author="Andrei Starichenko",
-  author_email="centroid@mail.ru",
-
+  version="0.2.0",
   description="Update class attributes from OsEnvironment or IniFile",
-  long_description=readme,
-  long_description_content_type="text/markdown",
   keywords=["environs", "environment", "rc", "ini", "private", "test several 123"],
-
-  # url="https://github.com/centroid457/private_values",
-  project_urls={
-    # "Documentation": "https://github.com/centroid457/private_values/blob/main/GUIDE.md",
-    "Source": "https://github.com/centroid457/private_values",
-  },
-
-  packages=[NAME, ],
-  install_requires=[],
   classifiers=[
     # "Framework :: ",
     "Topic :: Security",
     "Topic :: Software Development :: Libraries :: Python Modules",
     "Topic :: System :: Logging",
+
+    # EDIT ============================================================================================================
+    # EDIT ============================================================================================================
+    # EDIT ============================================================================================================
+    # EDIT ============================================================================================================
+    # EDIT ============================================================================================================
+
     "Development Status :: 5 - Production/Stable",
     "License :: OSI Approved :: MIT License",
     "Programming Language :: Python",
@@ -40,5 +38,22 @@ setup(
     "Natural Language :: English",
     "Typing :: Typed",
   ],
+
+  name=NAME,
+  author="Andrei Starichenko",
+  author_email="centroid@mail.ru",
+  long_description=readme,
+  long_description_content_type="text/markdown",
+
+  url="https://github.com/centroid457/",  # HOMEPAGE
+  project_urls={
+    # "Documentation": f"https://github.com/centroid457/{NAME}/blob/main/GUIDE.md",
+    "Source": f"https://github.com/centroid457/{NAME}",
+  },
+
+  packages=[NAME, ],
+  install_requires=[],
   python_requires=">=3.6"
 )
+
+# =====================================================================================================================
