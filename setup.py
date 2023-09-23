594bc817803587d3260cd32f3c82eaa98ed9fbaa
admin <023>
2023-09-23 22:59:19 +0300

    ready v0.4.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as f:
 NAME = "private_values"
 
 setup(
-  version="0.4.0",
+  version="0.4.1",
   description="Get values from OsEnvironment or Ini/Json File",
   keywords=[
       "environs", "environment",
