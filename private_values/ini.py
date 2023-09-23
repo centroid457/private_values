340e4718b7a1e0adb3a26784c5fcf85b0cad4f28
admin <023>
2023-09-23 23:18:07 +0300

    use in INI self.SECTION = None for DEFAULT

--- private_values/ini.py
+++ private_values/ini.py
@@ -6,10 +6,8 @@ from configparser import ConfigParser
 # =====================================================================================================================
 class PrivateIni(PrivateBase):
     """
-    read exact section from IniFile
     Not recommended using DEFAULT SECTION!
     """
-    SECTION: str = "DEFAULT"
     FILENAME: str = "pv.ini"
 
     def get_as_dict(self) -> Union[Type_PvDict, NoReturn]:
@@ -21,8 +19,8 @@ class PrivateIni(PrivateBase):
             msg = f"[CRITICAL] incorrect format file!\n{exx!r}"
             raise Exx_PvNotAccepted(msg)
 
-        if self.SECTION == "DEFAULT" or ini.has_section(section=self.SECTION):
-            result = dict(ini[self.SECTION])
+        if not self.SECTION or self.SECTION == "DEFAULT" or ini.has_section(section=self.SECTION):
+            result = dict(ini[self.SECTION or "DEFAULT"])
             return result
         else:
             msg = f"[CRITICAL] NO [{self.SECTION=} in {self.filepath=}]\n"
