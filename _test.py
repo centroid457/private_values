b09255c1bdb29728a1720bdc31b9017d2c5506c0
a.starichenko <centroid@mail.ru>
2023-10-25 15:31:40 +0300

    add tests for PrivateCsv

--- _test.py
+++ _test.py
@@ -12,7 +12,7 @@ from annot_attrs import *
 
 
 # =====================================================================================================================
-# TODO: merge Ini/Json tests via parametrisation
+# TODO: merge Csv/Ini/Json tests via parametrisation
 
 
 # =====================================================================================================================
@@ -66,6 +66,68 @@ class Test__Env:
         print(envs)
         assert envs.get(self.NAME_Exists.upper()) == self.VALUE
 
+
+# =====================================================================================================================
+class Test__Csv:
+    VICTIM: Type[PrivateCsv] = type("VICTIM", (PrivateCsv,), {})
+    VICTIM2_FILENAME: str = f"{PrivateCsv.FILENAME}2"
+    VICTIM2: Type[PrivateCsv] = type("VICTIM2", (PrivateCsv,), {"FILENAME": VICTIM2_FILENAME})
+    DIRPATH: pathlib.Path = pathlib.Path(TemporaryDirectory().name)
+
+    TEXT1: str = f"""
+name:111
+name1:111
+    """
+    TEXT2: str = f"""
+name:222
+name2:222
+    """
+    @classmethod
+    def setup_class(cls):
+        cls.DIRPATH.mkdir()
+        cls.DIRPATH.joinpath(cls.VICTIM.FILENAME).write_text(cls.TEXT1)
+        cls.DIRPATH.joinpath(cls.VICTIM2.FILENAME).write_text(cls.TEXT2)
+
+    @classmethod
+    def teardown_class(cls):
+        shutil.rmtree(cls.DIRPATH)
+
+    def setup_method(self, method):
+        self.VICTIM = type("VICTIM", (PrivateCsv,), {})
+        self.VICTIM2 = type("VICTIM2", (PrivateCsv,), {"FILENAME": self.VICTIM2_FILENAME})
+        self.VICTIM.DIRPATH = self.DIRPATH
+        self.VICTIM2.DIRPATH = self.DIRPATH
+
+    # -----------------------------------------------------------------------------------------------------------------
+    def test__notExist_file(self):
+        self.VICTIM.FILENAME = "12345.csv"
+
+        try:
+            self.VICTIM()
+        except Exx_FileNotExists:
+            pass
+        else:
+            assert False
+
+    def test__notExist_name(self):
+        assert self.VICTIM().nAme == "111"
+
+        try:
+            self.VICTIM().name999
+        except Exx_AttrNotExist:
+            pass
+        else:
+            assert False
+
+    def test__use_init(self):
+        assert self.VICTIM().name == "111"
+        assert self.VICTIM(_filepath=pathlib.Path(self.VICTIM2.DIRPATH, self.VICTIM2_FILENAME)).name == "222"
+        assert self.VICTIM(_filename=self.VICTIM2_FILENAME).name == "222"
+
+        assert self.VICTIM(_text="name11:11").name11 == "11"
+
+        self.VICTIM.SEPARATOR = "="
+        assert self.VICTIM(_text="name11=11").name11 == "11"
 
 # =====================================================================================================================
 class Test__Ini:
